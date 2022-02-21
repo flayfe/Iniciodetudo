87b8740d25c12b7ebeb0413d002a40f8668646cf
Flavia Santos <88909838+flayfe@users.noreply.github.com>
2022-02-21 16:57:52 -0300

    Add files via upload

---
+++ numerosPares.java
@@ -0,0 +1,23 @@
+/* Desafio:
+Crie um programa que leia um número e mostre os números pares até esse número, inclusive ele mesmo.
+
+Entrada
+Você receberá 1 valor inteiro N, onde N > 0.
+Saída
+Exiba todos os números pares até o valor de entrada, sendo um em cada linha.
+*/
+
+import java.util.Scanner;
+
+public class numerosPares{
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int num = scan.nextInt();
+
+        for (int i = 1 ; i <= num ; i++) {
+            if (i % 2 == 0 ) System.out.println(i);
+        }
+    }
+
+}
